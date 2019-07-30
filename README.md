9ccb7d7e88494325994264bbacc6247dd0427a72
Matthias P. Braendli <matthias.braendli@mpb.li>
2019-07-30 15:02:46 +0200

    Add files taken from ODR-DabMux and ODR-DabMod

---
+++ README.md
@@ -0,0 +1,21 @@
+Pieces of code common to all ODR-mmbtools
+=========================================
+
+Contains:
+
+- Character set conversion
+- Socket abstraction
+- Reed Solomon
+- CRC routines
+- ThreadsafeQueue
+- EDI input and output code
+- TAI bulletin download
+- Log output
+- Remotecontrol
+
+LICENCE
+=======
+
+GPLv3+
+
+All files in fec are licenced under the LGPL v2.1, please see LICENCE
---
+++ main.cpp
@@ -0,0 +1,51 @@
+/* Testing file
+ *
+   Copyright (C) 2019
+   Matthias P. Braendli, matthias.braendli@mpb.li
+
+   A test main.
+ */
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include "ClockTAI.h"
+#include "Log.h"
+#include "ReedSolomon.h"
+#include "RemoteControl.h"
+#include "charset/charset.h"
+#include "edi/STIDecoder.hpp"
+#include "edi/STIWriter.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ClockTAI ct({"https://www.ietf.org/timezones/data/leap-seconds.list", "https://raw.githubusercontent.com/eggert/tz/master/leap-seconds.list"});
+    etiLog.level(info) << "TAI offset = " << ct.get_offset();
+
+    ReedSolomon rs(255, 207);
+
+    RemoteControllerTelnet telnet;
+    etiLog.level(info) << "RC telnet started";
+
+    rcs.enrol(&ct);
+
+    CharsetConverter charset_converter;
+    charset_converter.utf8_to_ebu("From UTF to EBU", false);
+
+    EdiDecoder::STIWriter edi_stiwriter;
+    EdiDecoder::STIDecoder edi_decoder(edi_stiwriter, true);
+}
